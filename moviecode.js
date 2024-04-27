43588eb86cdc6245dc627d15dc2502d7e314ace7
SuhyeonJin <lofi0613@gmail.com>
2024-04-27 14:47:28 +0900

    카드버튼클릭?

--- moviecode.js
+++ moviecode.js
@@ -2,14 +2,16 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: ''
+        Authorization: 'application/json',
+        
     }
 };
 
 let movie;
 let searchedMovie;
+let movieCards;
 
-let printtitle = function () {
+let printtitle = () => {
     return (data) => {
         movie = data['results'];
         movie.forEach((a) => {
@@ -18,67 +20,93 @@ let printtitle = function () {
             let movieOverview = a['overview'];
             let movieImage = a['poster_path'];
             let movieCardHTML = `
-            <div class="card">
+            <input type="button" class="cardbtn" id="button${movieIndex}">
+            <label for="button${movieIndex}" class="card" id="card${movieIndex}">
                <div class="imagesection">
                     <img class="cardimage" id="carimage" 
                         src="https://image.tmdb.org/t/p/original${movieImage}" alt="...">
                 </div>
                 <div class="textsection">
-                    <h5 class="cardtitle" id="${movieIndex}">${movieTitle}</h5>
+                    <h5 class="cardtitle" id="title${movieIndex}">${movieTitle}</h5>
                     <p class="cardtext">${movieOverview}'</p>
                 </div>
-            </div>`
+            </label>`
+
+
+
             document.querySelector("#cardrow" + movieIndex % 3).innerHTML += movieCardHTML;
         })
+        movieCards = document.querySelectorAll(".cardbtn");
+        //resolve(movieCards);
     }
-    
+
 }
 
+let loading = function () {
+    window.onload = () => {
+        const btn = document.querySelector("#searchbtn");
+        btn.addEventListener("click", clickBtn);
 
+        document.querySelector('#input').addEventListener("keydown", (e) => {
+            if (e.code == 'Enter') {
+                btn.click();
+            }
+        })
+        
+    }
+}
 
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(printtitle())
+    .then(loading())
     .catch(err => console.error(err));
 
 
-let clickbtn = function () {
+let clickBtn = () => {
     searchedMovie = [];
-    let a = document.querySelector("#input").value.toLowerCase();
-    console.log("입력값 : " + a);
-    movie.forEach(function (i) {
+    const a = document.querySelector("#input").value.toLowerCase();
+    movie.forEach((i) => {
         let title = i["title"];
         if (title.toLowerCase().includes(a)) {
             searchedMovie.push(i);
         }
     })
-    document.querySelectorAll(".cardrow").forEach( function (i) {
+    document.querySelectorAll(".cardrow").forEach(function (i) {
         i.innerHTML = ``;
     })
-    searchedMovie.forEach( function(a) {
+    searchedMovie.forEach((a) => {
         let movieIndex = searchedMovie.indexOf(a)
-            let movieTitle = a['title'];
-            let movieOverview = a['overview'];
-            let movieImage = a['poster_path'];
-            let movieCardHTML = `
-            <div class="card">
-               <div class="imagesection">
-                    <img class="cardimage" id="carimage" 
-                        src="https://image.tmdb.org/t/p/original${movieImage}" alt="...">
-                </div>
-                <div class="textsection">
-                    <h5 class="cardtitle" id="${movieIndex}">${movieTitle}</h5>
-                    <p class="cardtext">${movieOverview}'</p>
-                </div>
-            </div>`
-            document.querySelector("#cardrow" + movieIndex % 3).innerHTML += movieCardHTML;
-        })    
+        let movieTitle = a['title'];
+        let movieOverview = a['overview'];
+        let movieImage = a['poster_path'];
+        let movieCardHTML = `
+        <input type="button" class="cardbtn" id="button${movieIndex}">
+        <label for="button${movieIndex}" class="card" id="card${movieIndex}">
+           <div class="imagesection">
+                <img class="cardimage" id="carimage" 
+                    src="https://image.tmdb.org/t/p/original${movieImage}" alt="...">
+            </div>
+            <div class="textsection">
+                <h5 class="cardtitle" id="title${movieIndex}">${movieTitle}</h5>
+                <p class="cardtext">${movieOverview}'</p>
+            </div>
+        </label>`
+        document.querySelector("#cardrow" + movieIndex % 3).innerHTML += movieCardHTML;
+    })
 }
 
-window.onload = function () {
-    let btn = document.querySelector("#searchbtn");
-    btn.addEventListener("click", clickbtn);
-}
+
+// window.onload = () => {
+//     const btn = document.querySelector("#searchbtn");
+//     btn.addEventListener("click", clickBtn);
+
+//     document.querySelector('#input').addEventListener("keydown", (e) => {
+//         if (e.code == 'Enter') {
+//             btn.click();
+//         }
+//     })
+// }
 
 
 /* data['results'] 내부 키들...
